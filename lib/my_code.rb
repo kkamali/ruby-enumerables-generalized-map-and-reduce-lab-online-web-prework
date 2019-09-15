6f28611ae1ba0bcc1d7b2d11ca6a1d41696f35fe
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 20:06:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,9 +10,9 @@ end
 
 def reduce(source_array, starting_point = 0)
   counter = 0
-  keep = 0
+  keep = starting_point
   while counter < source_array.length do
-    keep += yield(starting_point, source_array[counter])
+    keep += yield(source_array[counter])
     counter += 1
   end
   keep
