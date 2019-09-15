82cf4e7dfa9e06e3403bac287ff636f06d1d68fa
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-15 20:13:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point = 1)
+def reduce(source_array, starting_point*)
   counter = 0
   total = starting_point
   while counter < source_array.length do
